1c934c2a4dbc6420d507f5af128c2e8448cc03f7
Tharun310 <kondurutharun666@gmail.com>
2023-09-21 07:12:39 +0530

    work on admin

--- fe/src/Layout/Admin/AdminDashboard.js
+++ fe/src/Layout/Admin/AdminDashboard.js
@@ -2,6 +2,30 @@ import React from "react";
 
 import { ToastContainer } from "react-toastify";
 import { CheckAdminComponent } from "../../Service/AdminSecurity";
+import { getTeams } from "../../Service/adminapi";
+
+
+const loadData = async () => {
+  let getTeamDetails = await getTeams();
+  console.log(getTeamDetails);
+  try {
+    if (getTeamDetails.status === 200) {
+      setOriginalEventsData(
+        getEventDetails.data.upcomingEvent.filter((e) => e.isDelete === "0")
+      );
+      setEvents(
+        getEventDetails.data.upcomingEvent.filter((e) => e.isDelete === "0")
+      );
+    } else {
+      setOriginalEventsData("");
+      setEvents("");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
 function AdminDashboard() {
   return (
     <div className="sidebar-content-separation">
--- fe/src/Service/adminapi.js
+++ fe/src/Service/adminapi.js
@@ -3,19 +3,40 @@ import { env } from "../env";
 
 
 
+export const getEvent = async () => {
+  let getAllEvent = await axios
+    .get(`${env.REACT_APP_API}/dashboard/upcoming_event/get`)
+    .then((res) => {
+      return res;
+    })
+    .catch((error) => {
+      return error.response;
+    });
+  return getAllEvent;
+};
+
+
 // Fetch teams from the server
-export const fetchTeams = () => {
-  return axios.get(`${BASE_URL}/teams`);
+export const fetchTeams = async () => {
+  let getTeams = await axios
+  .get(`${env.REACT_APP_API}/dashboard/teams/get`)
+  .then((res) => {
+    return res;
+  })
+  .catch((error) => {
+    return error.response;
+  });
+return getTeams;
 };
 
 // Fetch matches from the server
 export const fetchMatches = () => {
-  return axios.get(`${BASE_URL}/matches`);
+  return axios.get(`${env.REACT_APP_API}/matches`);
 };
 
 // Update the scorecard for a specific match and team
 export const updateScorecard = (matchId, teamId, data) => {
-  return axios.post(`${BASE_URL}/matches/${matchId}/scorecard`, {
+  return axios.post(`${env.REACT_APP_API}/matches/${matchId}/scorecard`, {
     teamId,
     ...data,
   });
